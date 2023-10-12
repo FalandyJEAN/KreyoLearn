881ebca4972bffb7036850ce73a91a8e6fbf2ff8
Falandy <Falandyjean@gmail.com>
2023-10-12 00:50:21 -0400

    update

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("scroll", function() {
     const header = document.querySelector("header.scroll")
-    if (window.scrollY > 100) {
+    if (window.scrollY > 10) {
         header.classList.add("scrolled")
     } else {
         header.classList.remove("scrolled")
